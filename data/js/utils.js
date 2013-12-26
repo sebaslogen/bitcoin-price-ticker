3c07d2982918314ad0b109066c8c10abdc80dddf
neoranga55 <neoranga55@gmail.com>
2013-12-26 16:09:16 +0100

    Firefox content standards + show changelists
    - Big code refactor to use Firefox content standards
    - Improved number of requests to exchanges
    - Changelist displayed after new Addon updates

--- data/js/utils.js
+++ data/js/utils.js
@@ -1,7 +1,13 @@
 
-// Update content and style of ticker widget //
-self.port.on("update", function(color, font_size, new_content) {
-  $('#ticker-data').text(new_content);
+// Update content of ticker widget //
+self.port.on("updateContent", function(new_content) {
+  if (new_content != null) {
+    $('#ticker-data').text(new_content);
+  }
+});
+
+// Update and style of ticker widget //
+self.port.on("updateStyle", function(color, font_size, new_content) {
   $('#ticker-data').css('font-size', font_size);
   $('#ticker-data').css('color', color);
 });
