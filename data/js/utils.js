eba09c908c6d941e25c5914140c1df3941ee3ddb
neoranga55 <neoranga55@gmail.com>
2013-12-28 19:09:38 +0100

    Added Litecoin and ticker backgrounds
    - Added Litecoin
    - Ticker backgrounds (gold and silver)
    - Simplification of settings
    - Text measurement code (disabled)

--- data/js/utils.js
+++ data/js/utils.js
@@ -3,11 +3,23 @@
 self.port.on("updateContent", function(new_content) {
   if (new_content != null) {
     $('#ticker-data').text(new_content);
+    /*console.log("content: " + new_content + "-width:" + $('#ticker-data').width());
+    self.port.emit("updateTickerWidth", $('#ticker-data').width());*/
   }
 });
 
 // Update and style of ticker widget //
-self.port.on("updateStyle", function(color, font_size, new_content) {
+self.port.on("updateStyle", function(color, font_size, gold_background, silver_background) {
   $('#ticker-data').css('font-size', font_size);
   $('#ticker-data').css('color', color);
+  if (gold_background) {
+    $('#ticker-data').removeClass('silver-background');
+    $('#ticker-data').addClass('golden-background');
+  } else if (silver_background) {
+    $('#ticker-data').removeClass('golden-background');
+    $('#ticker-data').addClass('silver-background');
+  } else {
+    $('#ticker-data').removeClass('golden-background');
+    $('#ticker-data').removeClass('silver-background');
+  }
 });
