1956796f9d02287193c0b6f07c0f86b6ed2781d4
neoranga55 <neoranga55@gmail.com>
2014-02-12 21:30:26 +0100

    Add background to other alt-coins
    - Refactor code for background color selection based on ticker ID
    - New option to enable background on other alt-coins
    - Added new background CSS to each cryptocurrency

--- data/js/utils.js
+++ data/js/utils.js
@@ -7,17 +7,13 @@ self.port.on("updateContent", function(new_content) {
 });
 
 // Update and style of ticker widget //
-self.port.on("updateStyle", function(color, font_size, gold_background, silver_background) {
+self.port.on("updateStyle", function(color, font_size, background_color) {
   $('#ticker-data').css('font-size', font_size);
   $('#ticker-data').css('color', color);
-  if (gold_background) {
-    $('#ticker-data').removeClass('silver_background');
-    $('#ticker-data').addClass('gold_background');
-  } else if (silver_background) {
-    $('#ticker-data').removeClass('gold_background');
-    $('#ticker-data').addClass('silver_background');
+  if (background_color) {
+    $('#ticker-data').removeClass();
+    $('#ticker-data').addClass(background_color);
   } else {
-    $('#ticker-data').removeClass('gold_background');
-    $('#ticker-data').removeClass('silver_background');
+    $('#ticker-data').removeClass();
   }
 });
