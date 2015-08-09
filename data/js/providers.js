9a9ae2c9fbf877cbc5f053b8dc410e034709c892
Sebas LG <neoranga55@gmail.com>
2015-08-09 18:16:43 +0200

    First half of provider data migrated

--- data/js/providers.js
+++ data/js/providers.js
@@ -1,5 +1,5 @@
 var tickersRepository = {
-  // US Dollar prices //
+  // US Dollar prices
   'BitStampUSD': { exchangeName:'BitStamp', currency:'$', baseCurrency:'\u0243', url:"https://www.bitstamp.net/api/ticker/", jsonPath:['last']},
   'BTCeUSD': { exchangeName:'BTCe', currency:'$', baseCurrency:'\u0243', url:"https://btc-e.com/api/2/btc_usd/ticker", jsonPath:['ticker','last']},
   'KrakenUSD': { exchangeName:'Kraken', currency:'$', baseCurrency:'\u0243', url:"https://api.kraken.com/0/public/Ticker?pair=XBTUSD", jsonPath:['result','XXBTZUSD','c',0]},
@@ -9,35 +9,44 @@ var tickersRepository = {
   'BitPayUSD': { exchangeName:'BitPay', currency:'$', baseCurrency:'\u0243', url:"https://bitpay.com/api/rates", jsonPath:[1,'rate']},
   'TheRockTradingUSD': { exchangeName:'TheRockTrading', currency:'$', baseCurrency:'\u0243', url:"https://www.therocktrading.com/api/ticker/BTCUSD", jsonPath:['result',0,'last']},
   'BitFinexUSD': { exchangeName:'BitFinex', currency:'$', baseCurrency:'\u0243', url:"https://api.bitfinex.com/v1/ticker/btcusd", jsonPath:['last_price']},
-  // Euro prices //
-/*  'BTCeEUR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'KrakenEUR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'CoinDeskEUR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BitPayEUR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BitonicEUR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'Bitcoin-CentralEUR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'TheRockTradingEUR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'CoinDeskGBP': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'LocalbitcoinsGBP': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BittyliciousGBP': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BitPayGBP': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BitcurexPLN': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'CaVirTexCAD': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BTCChinaCNY': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'MercadoBitcoinBRL': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BTCTurkTRY': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BitcoinVenezuelaVEF': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'LocalbitcoinsVEF': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'CoinbaseVEF': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BitcoinVenezuelaARS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'LocalbitcoinsARS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'CoinbaseARS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BitexARS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'LocalbitcoinsCLP': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BitsoMXN': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'BitXZAR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'CoinbaseZAR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
-  'Bit2CILS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
+  // Euro prices
+  'BTCeEUR': { exchangeName:'BTCe', currency:'\u20ac', baseCurrency:'\u0243', url:"https://btc-e.com/api/2/btc_eur/ticker", jsonPath:['ticker','last']},
+  'KrakenEUR': { exchangeName:'Kraken', currency:'\u20ac', baseCurrency:'\u0243', url:"https://api.kraken.com/0/public/Ticker?pair=XBTEUR", jsonPath:['result','XXBTZEUR','c',0]},
+  'CoinDeskEUR': { exchangeName:'CoinDesk', currency:'\u20ac', baseCurrency:'\u0243', url:"http://api.coindesk.com/v1/bpi/currentprice.json", jsonPath:['bpi','EUR','rate_float']},
+  'BitPayEUR': { exchangeName:'BitPay', currency:'\u20ac', baseCurrency:'\u0243', url:"https://bitpay.com/api/rates", jsonPath:[2,'rate']},
+  'BitonicEUR': { exchangeName:'Bitonic', currency:'\u20ac', baseCurrency:'\u0243', url:"https://bitonic.nl/api/price", jsonPath:['price']},
+  'Bitcoin-CentralEUR': { exchangeName:'Bitcoin-Central', currency:'\u20ac', baseCurrency:'\u0243', url:"https://bitcoin-central.net/api/v1/data/eur/ticker", jsonPath:['price']},
+  'TheRockTradingEUR': { exchangeName:'TheRockTrading', currency:'\u20ac', baseCurrency:'\u0243', url:"https://www.therocktrading.com/api/ticker/BTCEUR", jsonPath:['result',0,'last']},
+  // Pound prices
+  'CoinDeskGBP': { exchangeName:'CoinDesk', currency:'\u00a3', baseCurrency:'\u0243', url:"http://api.coindesk.com/v1/bpi/currentprice.json", jsonPath:['bpi','GBP','rate_float']},
+  'LocalbitcoinsGBP': { exchangeName:'Localbitcoins', currency:'\u00a3', baseCurrency:'\u0243', url:"https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/", jsonPath:['GBP','rates','last']},
+  'BittyliciousGBP': { exchangeName:'Bittylicious', currency:'\u00a3', baseCurrency:'\u0243', url:"https://bittylicious.com/api/v1/quote/BTC/GB/GBP/BANK/1", jsonPath:['totalPrice']},
+  'BitPayGBP': { exchangeName:'BitPay', currency:'\u00a3', baseCurrency:'\u0243', url:"https://bitpay.com/api/rates", jsonPath:[3,'rate']},
+  // Polish zloty
+  'BitcurexPLN': { exchangeName:'Bitcurex', currency:'z\u0141', baseCurrency:'\u0243', url:"https://bitcurex.com/api/pln/ticker.json", jsonPath:['last_tx_price']},
+  // Canadian dollar prices
+  'CaVirTexCAD': { exchangeName:'CaVirTex', currency:'C$', baseCurrency:'\u0243', url:"https://www.cavirtex.com/api/CAD/ticker.json", jsonPath:['last']},
+  // Yuan prices
+  'BTCChinaCNY': { exchangeName:'BTCChina', currency:'\u5143', baseCurrency:'\u0243', url:"https://data.btcchina.com/data/ticker", jsonPath:['ticker','last']},
+  // Ruble prices
+  'BTCeRUR': { exchangeName:'BTCe', currency:'RUR', baseCurrency:'\u0243', url:"https://btc-e.com/api/2/btc_rur/ticker", jsonPath:['ticker','last']}/*,
+  'MercadoBitcoinBRL': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'BTCTurkTRY': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+
+  'BitcoinVenezuelaVEF': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'LocalbitcoinsVEF': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'CoinbaseVEF': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'BitcoinVenezuelaARS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'LocalbitcoinsARS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'CoinbaseARS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'BitexARS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'LocalbitcoinsCLP': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'BitsoMXN': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'BitXZAR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  'CoinbaseZAR': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  // Shekel (Israel)
+  'Bit2CILS': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'\u0243', url:"XXXXX", jsonPath:XXXXX},
+  // Litecoin prices
   'BTCeLitecoin': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
   'KrakenLitecoin': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
   'VircurexLitecoin': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX},
@@ -63,61 +72,35 @@ var tickersRepository = {
   'PoloniexBurst': { exchangeName:'XXXXX', currency:'XXXXX', baseCurrency:'XXXXX', url:"XXXXX", jsonPath:XXXXX}*/
 };// Store all tickers here
 /*
-  setupTicker('BTCeEUR', 'BTCe', '\u20ac', '\u0243', '#013ADF', "https://btc-e.com/api/2/btc_eur/ticker", ['ticker','last']);
-  setupTicker('KrakenEUR', 'Kraken', '\u20ac', '\u0243', '#3366FF', "https://api.kraken.com/0/public/Ticker?pair=XBTEUR", ['result','XXBTZEUR','c',0]);
-  setupTicker('CoinDeskEUR', 'CoinDesk', '\u20ac', '\u0243', '#000066', "http://api.coindesk.com/v1/bpi/currentprice.json", ['bpi','EUR','rate_float']);
-  setupTicker('BitPayEUR', 'BitPay', '\u20ac', '\u0243', '#B43104', "https://bitpay.com/api/rates", [2,'rate']);
-  setupTicker('BitonicEUR', 'Bitonic', '\u20ac', '\u0243', '#B43104', "https://bitonic.nl/api/price", ['price']);
-  setupTicker('Bitcoin-CentralEUR', 'Bitcoin-Central', '\u20ac', '\u0243', '#B43104', "https://bitcoin-central.net/api/v1/data/eur/ticker", ['price']);
-  setupTicker('TheRockTradingEUR', 'TheRockTrading', '\u20ac', '\u0243', '#B43104', "https://www.therocktrading.com/api/ticker/BTCEUR", ['result',0,'last']);
-
-  // Pound prices //
-  setupTicker('CoinDeskGBP', 'CoinDesk', '\u00a3', '\u0243', '#088A08', "http://api.coindesk.com/v1/bpi/currentprice.json", ['bpi','GBP','rate_float']);
-  setupTicker('LocalbitcoinsGBP', 'Localbitcoins', '\u00a3', '\u0243', '#088A08', "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/", ['GBP','rates','last']);
-  setupTicker('BittyliciousGBP', 'Bittylicious', '\u00a3', '\u0243', '#088A08', "https://bittylicious.com/api/v1/quote/BTC/GB/GBP/BANK/1", ['totalPrice']);
-  setupTicker('BitPayGBP', 'BitPay', '\u00a3', '\u0243', '#FF0000', "https://bitpay.com/api/rates", [3,'rate']);
-
-  // Polish zloty //
-  setupTicker('BitcurexPLN', 'Bitcurex', 'z\u0141', '\u0243', '#B43104', "https://bitcurex.com/api/pln/ticker.json", ['last_tx_price']);
-  
-  // Canadian dollar prices //
-  setupTicker('CaVirTexCAD', 'CaVirTex', 'C$', '\u0243', '#B43104', "https://www.cavirtex.com/api/CAD/ticker.json", ['last']);
-
-  // Yuan prices //
-  setupTicker('BTCChinaCNY', 'BTCChina', '\u5143', '\u0243', '#DF01D7', "https://data.btcchina.com/data/ticker", ['ticker','last']);
-
-  // Ruble prices //
-  setupTicker('BTCeRUR', 'BTCe', 'RUR', '\u0243', '#4F3107', "https://btc-e.com/api/2/btc_rur/ticker", ['ticker','last']);
-
   // Brazilian Real prices //
-  setupTicker('MercadoBitcoinBRL', 'MercadoBitcoin', 'R$', '\u0243', '#2F4F15', "https://www.mercadobitcoin.com.br/api/ticker/", ['ticker','last']);
+  setupTicker('MercadoBitcoinBRL', 'MercadoBitcoin', 'R$', '', '#2F4F15', "https://www.mercadobitcoin.com.br/api/ticker/", ['ticker','last']);
 
   // Turkish Lira //
-  setupTicker('BTCTurkTRY', 'BTCTurk', '\u20ba', '\u0243', '#5B5B2F', "https://www.btcturk.com/api/ticker", ['last']);
+  setupTicker('BTCTurkTRY', 'BTCTurk', '\u20ba', '', '#5B5B2F', "https://www.btcturk.com/api/ticker", ['last']);
 
   // Venezuelan Bolivar //
-  setupTicker('BitcoinVenezuelaVEF', 'BitcoinVenezuela', 'Bs', '\u0243', '#672F7F', "http://bitcoinvenezuela.com/api/btcven.json", ['BTC','VEF']);
-  setupTicker('LocalbitcoinsVEF', 'Localbitcoins', 'Bs', '\u0243', '#088A08', "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/", ['VEF','rates','last']);
-  setupTicker('CoinbaseVEF', 'Coinbase', 'Bs', '\u0243', '#4C285B', "https://coinbase.com/api/v1/currencies/exchange_rates", ['btc_to_vef']);
+  setupTicker('BitcoinVenezuelaVEF', 'BitcoinVenezuela', 'Bs', '', '#672F7F', "http://bitcoinvenezuela.com/api/btcven.json", ['BTC','VEF']);
+  setupTicker('LocalbitcoinsVEF', 'Localbitcoins', 'Bs', '', '#088A08', "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/", ['VEF','rates','last']);
+  setupTicker('CoinbaseVEF', 'Coinbase', 'Bs', '', '#4C285B', "https://coinbase.com/api/v1/currencies/exchange_rates", ['btc_to_vef']);
 
   // Argentinian Peso //
-  setupTicker('BitcoinVenezuelaARS', 'BitcoinVenezuela', 'ARS$', '\u0243', '#783489', "http://bitcoinvenezuela.com/api/btcven.json", ['BTC','ARS']);
-  setupTicker('LocalbitcoinsARS', 'Localbitcoins', 'ARS$', '\u0243', '#088A08', "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/", ['ARS','rates','last']);
-  setupTicker('CoinbaseARS', 'Coinbase', 'ARS$', '\u0243', '#582266', "https://coinbase.com/api/v1/currencies/exchange_rates", ['btc_to_ars']);
-  setupTicker('BitexARS', 'Bitex', 'ARS$', '\u0243', '#783489', "https://bitex.la/api-v1/rest/btc/market/ticker", ['last']);
+  setupTicker('BitcoinVenezuelaARS', 'BitcoinVenezuela', 'ARS$', '', '#783489', "http://bitcoinvenezuela.com/api/btcven.json", ['BTC','ARS']);
+  setupTicker('LocalbitcoinsARS', 'Localbitcoins', 'ARS$', '', '#088A08', "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/", ['ARS','rates','last']);
+  setupTicker('CoinbaseARS', 'Coinbase', 'ARS$', '', '#582266', "https://coinbase.com/api/v1/currencies/exchange_rates", ['btc_to_ars']);
+  setupTicker('BitexARS', 'Bitex', 'ARS$', '', '#783489', "https://bitex.la/api-v1/rest/btc/market/ticker", ['last']);
 
   // Chilean Peso
-  setupTicker('LocalbitcoinsCLP', 'Localbitcoins', '$', '\u0243', '#088A08', "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/", ['CLP','rates','last']);
+  setupTicker('LocalbitcoinsCLP', 'Localbitcoins', '$', '', '#088A08', "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/", ['CLP','rates','last']);
 
   // Mexican Peso
-  setupTicker('BitsoMXN', 'Bitso', '$', '\u0243', '#088A08', "https://api.bitso.com/public/info", ['btc_mxn','rate']);
+  setupTicker('BitsoMXN', 'Bitso', '$', '', '#088A08', "https://api.bitso.com/public/info", ['btc_mxn','rate']);
 
   // South African Rand //
-  setupTicker('BitXZAR', 'BitX', 'R', '\u0243', '#4A1F54', "https://bitx.co.za/api/1/BTCZAR/ticker", ['last_trade']);
-  setupTicker('CoinbaseZAR', 'Coinbase', 'R', '\u0243', '#4A1F54', "https://coinbase.com/api/v1/currencies/exchange_rates", ['btc_to_zar']);
+  setupTicker('BitXZAR', 'BitX', 'R', '', '#4A1F54', "https://bitx.co.za/api/1/BTCZAR/ticker", ['last_trade']);
+  setupTicker('CoinbaseZAR', 'Coinbase', 'R', '', '#4A1F54', "https://coinbase.com/api/v1/currencies/exchange_rates", ['btc_to_zar']);
 
   // Shekel (Israel) //
-  setupTicker('Bit2CILS', 'Bit2C', '\u20AA', '\u0243', '#4A1F54', "https://www.bit2c.co.il/Exchanges/BtcNis/Ticker.json", ['ll']);
+  setupTicker('Bit2CILS', 'Bit2C', '\u20AA', '', '#4A1F54', "https://www.bit2c.co.il/Exchanges/BtcNis/Ticker.json", ['ll']);
 
   // Litecoin prices //
   setupTicker('BTCeLitecoin', 'BTCe', '\u0243', '\u0141', '#013ADF', "https://btc-e.com/api/2/ltc_btc/ticker", ['ticker','last']);
