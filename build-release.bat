8382f5b9c0009bd85fde933dd95a34fefa29ab8b
Sebas LG <neoranga55@gmail.com>
2015-08-20 00:07:34 +0200

    Create a build and release script

---
+++ build-release.bat
@@ -0,0 +1,5 @@
+del *.xpi
+cmd /k jpm xpi
+rename jid0-ziK34XHkBWB9ezxd4l9Q1yC7RP0*.xpi jid0-ziK34XHkBWB9ezxd4l9Q1yC7RP0@jetpack.xpi
+7z d jid0-ziK34XHkBWB9ezxd4l9Q1yC7RP0@jetpack.xpi "Node.js command prompt.lnk" build.bat
+pause
