197933fd9efe6f329b85f91ff4d595e13e8c3ad6
Sebas LG <neoranga55@gmail.com>
2015-08-20 16:03:55 +0200

    Fix release script

--- build-release.bat
+++ build-release.bat
@@ -1,5 +1,5 @@
 del *.xpi
-cmd /k jpm xpi
+cmd /c jpm xpi
 rename jid0-ziK34XHkBWB9ezxd4l9Q1yC7RP0*.xpi jid0-ziK34XHkBWB9ezxd4l9Q1yC7RP0@jetpack.xpi
 7z d jid0-ziK34XHkBWB9ezxd4l9Q1yC7RP0@jetpack.xpi "Node.js command prompt.lnk" build.bat
 pause
